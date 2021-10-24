d92e0e874ff47d7b399494542a06472556866c83
mm-ami <mhju.9599@gmail.com>
2021-10-25 01:49:39 +0900

    scrollAnimation 実装前まで

--- main.js
+++ main.js
@@ -1,7 +1,19 @@
-const menuIcon = document.querySelector('.menu-icon');
-const body = document.querySelector('body')
+document.addEventListener('DOMContentLoaded', function() {
+  const menuIcon = document.querySelector('.menu-icon');
+  const body = document.querySelector('body')
+  
+  menuIcon.addEventListener('click', () => {
+    menuIcon.classList.toggle('menu-open');
+  });
 
-menuIcon.addEventListener('click', () => {
-  menuIcon.classList.toggle('menu-open');
+  const slide = function(element, isIntersecting) {
+    if(isIntersecting) {
+      element.classList.add('inview');
+    }
+  };
+
+  const scroll = new ScrollObserver('', slide);
 })
+
+
 
